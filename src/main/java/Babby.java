707aa4cfc4059faea9d5f7205f3169b0085aa9b3
Julian <Julianlimwq12@Gmail.com>
2026-01-17 17:04:40 +0800

    Added more commands to list

--- src/main/java/Babby.java
+++ src/main/java/Babby.java
@@ -8,9 +8,9 @@ public class Babby {
         Scanner scanner = new Scanner(System.in); // Make scanner
 
         String logo = " ______        _     _           _ \n(____  \\      | |   | |         | |\n ____)  )_____| |__ | |__  _   _| |\n|  __  ((____ |  _ \\|  _ \\| | | |_|\n| |__)  ) ___ | |_) ) |_) ) |_| |_ \n|______/\\_____|____/|____/ \\__  |_|\n                          (____/   ";
-        System.out.println("Hello! I'm\n" + logo +"\nSo nice to meet you! Lets be friends <3\n----------------------------------\nWhat can I do for you?\n[bye] -> Exits the program\n");
-
-
+        System.out.println("Hello! I'm\n" + logo +"\nSo nice to meet you! Lets be friends <3\n----------------------------------\n");
+        // To do: Create a commands object to store the command, the purpose, and the code to run it
+        System.out.println("What can I do for you?\nlist -> Lists all tasks\nmark [task number] -> Marks the task as done\nunmark [task number] -> Marks the task as not done\nbye -> Exits the program\n");
 
         // Start echo
         while (true) {
