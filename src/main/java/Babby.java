e726f9233eeecc7e06e16718d0b7c4b61fd121bb
Julian <Julianlimwq12@Gmail.com>
2026-01-17 15:15:55 +0800

    Added new introduction

---
+++ src/main/java/Babby.java
@@ -0,0 +1,10 @@
+public class Babby {
+    public static void main(String[] args) {
+        String logo = "┳┓  ┓ ┓   ╻\n" +
+                "┣┫┏┓┣┓┣┓┓┏┃\n" +
+                "┻┛┗┻┗┛┗┛┗┫•\n" +
+                "         ┛ ";
+        System.out.println("Hello! I'm\n" + logo +"\nSo nice to meet you! Lets be friends <3");
+        System.out.println("See you again soon!");
+    }
+}
