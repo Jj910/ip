762d3dabcccabea7dd18717ae1e73bd664026221
Julian <Julianlimwq12@Gmail.com>
2026-01-17 15:36:40 +0800

    Added echo

--- src/main/java/Babby.java
+++ src/main/java/Babby.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Babby {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Make scanner
+
         String logo = "┳┓  ┓ ┓   ╻\n" +
                 "┣┫┏┓┣┓┣┓┓┏┃\n" +
                 "┻┛┗┻┗┛┗┛┗┫•\n" +
                 "         ┛ ";
-        System.out.println("Hello! I'm\n" + logo +"\nSo nice to meet you! Lets be friends <3");
+        System.out.println("Hello! I'm\n" + logo +"\nSo nice to meet you! Lets be friends <3\n----------------------------------\nWhat can I do for you?\n[bye] -> Exits the program");
+
+        // Start echo
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) break;
+            else System.out.println("\t"+input);
+        }
         System.out.println("See you again soon!");
     }
 }
