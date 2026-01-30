339cabd4efb1935e0269d469695068f1bf9cbf5b
Julian <Julianlimwq12@Gmail.com>
2026-01-30 09:05:27 +0800

    Add Javadocs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,39 +1,35 @@
-// The task class tracks the name of the task, and whether the class is complete. The order of the tasks are tracked by Babby
+/**
+ * Tracks the name of the task, and whether the task is complete. The order of the tasks are tracked by Babby.
+ */
 public abstract class Task {
     private final String title;
     private Boolean isComplete;
 
+    /**
+     * Creates a task with the title specified.
+     * The task is marked as incomplete by default.
+     *
+     * @param title Title of the task.
+     */
     public Task(String title) {
         this.title = title;
         this.isComplete = false;
     }
 
-    // Create a task and specify the completion status
+    /**
+     * Creates a task with specified completion status.
+     * @param title Title of the task.
+     * @param isComplete Completion status of the task. True if the task is complete, false otherwise.
+     */
     public Task(String title, Boolean isComplete) {
         this.title = title;
         this.isComplete = isComplete;
     }
 
-    public void setDone() {
-        this.isComplete = true;
-    }
-
-    public void setToDo() {
-        this.isComplete = false;
-    }
-
-    public Boolean getIsComplete() {
-        return this.isComplete;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
     /**
      * Encodes the task for it to be saved into the tasks file.
      *
-     * @return Encoded string of the task.
+     * @return Encoded string of the task in the format specific to the task type.
      */
     public abstract String toEncodedString();
 
