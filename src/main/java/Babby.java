7b90deec35aa7ebc4a922c8229fefbf5ba3cdfd5
Julian <Julianlimwq12@Gmail.com>
2026-01-30 09:15:28 +0800

    Move import statement

--- src/main/java/Babby.java
+++ src/main/java/Babby.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Babby {
     // List to store user input
